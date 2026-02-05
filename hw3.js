0fe535644d6fc44af14d74dfa678841422f68f00
jerrye987-bit <jerrye987@gmail.com>
2026-02-05 15:46:23 +0500

    Удален файл hw2. Создан файл hw3. Сделано первое задание.

---
+++ hw3.js
@@ -0,0 +1,6 @@
+// Задание 1
+let password = 'пароль';
+let userPassword = prompt('Введите пароль.');
+alert(password === userPassword ? "Пароль введен верно." : "Пароль введен неправильно.");
+
+// Задание 2
