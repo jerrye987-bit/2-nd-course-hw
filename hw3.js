90d55068cdee6f4dfd366ee2cdb6b617d9599b6b
jerrye987-bit <jerrye987@gmail.com>
2026-02-05 15:58:12 +0500

    Выполнено второе задание

--- hw3.js
+++ hw3.js
@@ -4,3 +4,9 @@ let userPassword = prompt('Введите пароль.');
 alert(password === userPassword ? "Пароль введен верно." : "Пароль введен неправильно.");
 
 // Задание 2
+let c = -3;
+if (c > 0 && c < 10) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
