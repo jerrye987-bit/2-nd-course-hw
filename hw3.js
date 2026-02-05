4312eb42a3efa3dcfcb0167e8b8cfecef81a7496
jerrye987-bit <jerrye987@gmail.com>
2026-02-05 16:16:01 +0500

    Выполнены задания 3, 4.

--- hw3.js
+++ hw3.js
@@ -4,9 +4,23 @@ let userPassword = prompt('Введите пароль.');
 alert(password === userPassword ? "Пароль введен верно." : "Пароль введен неправильно.");
 
 // Задание 2
-let c = -3;
+let c = Number(prompt('Введите любое число'));
 if (c > 0 && c < 10) {
     console.log("Верно");
 } else {
     console.log("Неверно");
 }
+
+// Задание 3
+let d = Number(prompt('Введите превое число'));
+let e = Number(prompt('Введите второе число'));
+if (d > 100 || e > 100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
+
+// Задание 4
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
