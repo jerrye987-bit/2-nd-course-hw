a0e217223078e0c2deb759670a06a416563b5ee6
jerrye987-bit <jerrye987@gmail.com>
2026-03-18 16:21:52 +0500

    Cделана игра 2

--- hw.js
+++ hw.js
@@ -20,3 +20,58 @@ function game1() {
         }
     }
 }
+
+
+function game2() {
+    function rndNum() {
+        return Math.floor(Math.random() * 10 + 1);
+    }
+    
+    const operations = ['+', '-', '*', '/'];
+    let rndOperationIndex = Math.floor(Math.random() * operations.length);
+    let rndOperation = operations[rndOperationIndex];
+    let num1 = rndNum();
+    let num2 = rndNum();
+
+    function rndTask() {
+        switch (rndOperation) {
+            case '+':
+                return num1 + num2;
+                break;
+            case '-':
+                if (num1 >= num2) {
+                    return num1 - num2;
+                } else {
+                    let num = num2;
+                    num2 = num1;
+                    num1 = num;
+                    return num1 - num2;
+                }
+                break;
+            case '*':
+                return num1 * num2;
+                break;
+            case '/':
+                let dividend = num1 * num2;
+                num1 = dividend;
+                return num1 / num2;
+                break;
+            default:
+                break;
+        }
+    }
+    
+    let answer = rndTask();
+    let userAnswer = prompt(`Реши задачу: ${num1} ${rndOperation} ${num2} = ?`);
+    if (userAnswer) {
+        let userAnswerNum = Number(userAnswer);
+
+        if (answer === userAnswerNum) {
+            alert(`Отлично! Ты верно решил задачу.`);
+        } else {
+            alert(`Задача решена не верно. Верный ответ ${answer}.`);
+        }
+    } else {
+        alert('Игра окончена.');
+    }
+}
